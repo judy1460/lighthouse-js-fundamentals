75c9f0826b8ff5a99e5bc0ad76849ea1c39e3d42
Mina <minatoosa@yahoo.com>
2020-10-09 23:56:13 +0000

    use if

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,16 @@
-const isCitizen = true;
-const age = 26;
-
-if (isCitizen && age > 18) {
-  console.log("You are eligible to vote.");
-}
+onst whichSchool  = function (age) {
+  if (age < 13) {
+    console.log("Elementary School");
+  } else if(13 < age < 18) {
+    console.log("Secondary School");
+  }
+  else if (age >18){
+    console.log("Lighthouse Labs");
+  } // Your code in here ...
+};
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
